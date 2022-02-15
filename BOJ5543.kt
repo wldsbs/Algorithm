cfa2676b256827cc8e5458606a837708d33f4c4a
jiyun <jiyun_99@naver.com>
2022-02-15 23:58:43 +0900

    Update BOJ5543.kt
    코드를 좀 더 간결하게 수정
    - 반복문 사용
    - 필요없는 변수 삭제

--- BOJ5543.kt
+++ BOJ5543.kt
@@ -3,19 +3,15 @@ import java.util.*
 fun main(args: Array<String>){
     val sc: Scanner = Scanner(System.`in`)
     var burger_list = ArrayList<Int>()
-    burger_list.add(sc.nextInt())
-    burger_list.add(sc.nextInt())
-    burger_list.add(sc.nextInt())
-    
-    var burger_min = burger_list.minOrNull()!!
-
     var juice_list = ArrayList<Int>()
-    juice_list.add(sc.nextInt())
-    juice_list.add(sc.nextInt())
-
-    var juice_min = juice_list.minOrNull()!!
-
-    val set_price = burger_min + juice_min - 50
     
-    println(set_price)
+    for(i in 0..2){
+        burger_list.add(sc.nextInt())
+    }
+    
+    for(i in 3..4){
+        juice_list.add(sc.nextInt())
+    }
+    
+    println(burger_list.minOrNull()!! + juice_list.minOrNull()!! - 50)
 }
