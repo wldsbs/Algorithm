cac7479503ac8bacf4dbc7c370b78db0c63f1423
jiyun <jiyun_99@naver.com>
2022-03-02 15:10:08 +0900

    improve: 백준 bronze2 문자열분석

--- BaekJoon/BOJ10820.kt
+++ BaekJoon/BOJ10820.kt
@@ -4,18 +4,18 @@ import kotlin.math.*
 fun main(){
     
     while(true){
-        val input = readLine()
+        val input = readLine() ?: return
         val result = IntArray(4,{0})
-        
-        input ?: return
-        
+                
         input.forEach{ it ->
-            if(it.toInt() > 96 && it.toInt() < 123){result[0]++}
-            else if(it.toInt() > 64 && it.toInt() < 91){result[1]++}
-            else if(it.toInt() > 47 && it.toInt() < 58){result[2]++} 
-            else if(it.toInt() == 32){result[3]++}
+            when(it){
+                in 'a'..'z' -> result[0]++
+                in 'A'..'Z' -> result[1]++
+                in '0'..'9' -> result[2]++
+                ' ' -> result[3]++
+            }
         }
-        
+
         for(i in 0..3){
             print(result[i].toString() + " ")
         }
