6929fbc887befb5a929bbcbab1f985171fd8d00f
jiyun <jiyun_99@naver.com>
2022-05-11 23:11:22 +0900

    solve: PGS / Lv1 / 행렬의덧셈

---
+++ Programmers/Lv1_행렬의덧셈.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+        var answer = Array<IntArray>(arr1.size){i -> 
+            IntArray(arr1[0].size){j -> arr1[i][j] + arr2[i][j]} }
+        
+        return answer
+    }
+}
